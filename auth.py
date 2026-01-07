36efdf3cf6336a4793c0db54391e554d9a621f5d
D lokesh reddy <dlokeshreddy3074@gmail.com>
2026-01-07 10:38:07 +0530

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+USERS = {
+    "hr": {"password": "hr123", "role": "HR"},
+    "employee": {"password": "emp123", "role": "Employee"}
+}
+
+def login():
+    st.subheader("🔐 Login")
+
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+
+    if st.button("Login"):
+        user = USERS.get(username)
+        if user and user["password"] == password:
+            st.session_state.logged_in = True
+            st.session_state.username = username
+            st.session_state.role = user["role"]
+            st.rerun()
+        else:
+            st.error("Invalid credentials")
